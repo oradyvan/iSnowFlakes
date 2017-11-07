2f90cabd9e490b0efadfd97a5c91ea9582ef11e7
Oleksiy Radyvanyuk <oleksiy@ngti.nl>
2017-11-07 11:00:28 +0100

    Add draft of unit tests target to the project.

---
+++ SnowflakesMakerTests/SnowflakesMakerTests.swift
@@ -0,0 +1,27 @@
+//
+//  SnowflakesMakerTests.swift
+//  SnowflakesMakerTests
+//
+//  Created by Oleksiy Radyvanyuk on 07/11/2017.
+//  Copyright © 2017 Oleksiy Radyvanyuk. All rights reserved.
+//
+
+import XCTest
+
+class SnowflakesMakerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+}
