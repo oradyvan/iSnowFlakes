22c45d32adbcf2f5cd650dcc0e64f03ba6490eb0
Oleksiy Radyvanyuk <oleksiy@ngti.nl>
2017-11-07 12:59:13 +0100

    Add failing unit test.

---
+++ SnowflakesMaker/SnowflakesMaker-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  SnowflakesMaker-Bridging-Header.h
+//  SnowflakesMaker
+//
+//  Created by Oleksiy Radyvanyuk on 07/11/2017.
+//  Copyright © 2017 Oleksiy Radyvanyuk. All rights reserved.
+//
+
+#ifndef SnowflakesMaker_Bridging_Header_h
+#define SnowflakesMaker_Bridging_Header_h
+
+#import "SnowflakesMaker.h"
+
+#endif /* SnowflakesMaker_Bridging_Header_h */
