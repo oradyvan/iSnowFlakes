aa2ea5b432fa7d0b7ebecbcd8aec4057e9f93856
Oleksiy Radyvanyuk <oradyvan@gmail.com>
2025-07-19 11:33:11 +0200

    Start migrating to Swift

--- SnowflakesMaker/SceneDelegate.swift
+++ SnowflakesMaker/SceneDelegate.swift
@@ -2,12 +2,17 @@ import UIKit
 
 final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
+    var window: UIWindow?
+
     func scene(
         _ scene: UIScene,
         willConnectTo session: UISceneSession,
         options connectionOptions: UIScene.ConnectionOptions
     ) {
         guard let windowScene = scene as? UIWindowScene else { return }
-        print(windowScene)
+        let window = UIWindow(windowScene: windowScene)
+        window.rootViewController = MainViewController()
+        self.window = window
+        window.makeKeyAndVisible()
     }
 }
