1c29f9f7eb6a982c5e06f0b9f64b1baa37fc1167
Oleksiy Radyvanyuk <oradyvan@gmail.com>
2025-11-07 19:33:51 +0100

    Add a switch for light/dark scheme.

--- iSnowFlakesDemoApp/iSnowFlakesApp.swift
+++ iSnowFlakesDemoApp/iSnowFlakesApp.swift
@@ -2,9 +2,23 @@ import SwiftUI
 
 @main
 struct iSnowFlakesApp: App {
+    @State var selection: Int = 0
+
     var body: some Scene {
         WindowGroup {
-            MainView()
+            ZStack {
+                MainView()
+                VStack {
+                    Picker("Theme", selection: $selection) {
+                        Text("Light").tag(0)
+                        Text("Dark").tag(1)
+                    }
+                    .pickerStyle(.segmented)
+                    .padding()
+                    Spacer()
+                }
+            }
+            .preferredColorScheme(selection == 1 ? ColorScheme.dark : nil)
         }
     }
 }
